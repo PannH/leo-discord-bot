4d4209445ee7cee639b55aad4c2e07c328df7184
PannH <pannh2609@gmail.com>
2022-08-20 01:27:00 +0200

    :sparkles: Make the permission names user-friendly

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -13,19 +13,21 @@ export default new Event('interactionCreate', async (client, interaction: Comman
          const command: Command = client.handlers.commands.cache.find((c) => c.data.name === interaction.commandName);
          const ctx: CommandContext = new CommandContext(client, interaction);
 
+         const perms = require('../../../data/permissions.json');
+
          const clientMissingPerms = command.data.clientPermissions.filter(
             (perm) => !interaction.guild.members.cache.get(client.user.id).permissions.has(perm)
          );
 
          if (!!clientMissingPerms.length)
-            return void ctx.errorReply('Missing Permission', `I require the following permission(s) to run this command: ${clientMissingPerms.map((p) => `\`${p}\``).join(', ')}`);
+            return void ctx.errorReply('Missing Permission', `I require the following permission(s) to run this command: ${clientMissingPerms.map((p) => `\`${perms[p.toString()].name}\``).join(', ')}`);
 
          const memberMissingPerms = command.data.memberPermissions.filter(
             (perm) => !interaction.memberPermissions.has(perm)
          );
 
          if (!!memberMissingPerms.length)
-            return void ctx.errorReply('Missing Permission', `You must have the following permission(s) to use this command: ${memberMissingPerms.map((p) => `\`${p}\``).join(', ')}`);
+            return void ctx.errorReply('Missing Permission', `You must have the following permission(s) to use this command: ${memberMissingPerms.map((p) => `\`${perms[p.toString()].name}\``).join(', ')}`);
 
          command.run(ctx);
 
